4cba70bd9a7ffc3c762b3dac045a33ecebe19f2b
Kim Blanquera <kim.blanquera@gmail.com>
2015-05-26 14:27:05 +0800

    Replacing bootstrap files and adding index layout

--- index.php
+++ index.php
@@ -3,22 +3,65 @@
   <head>
     <title>Twitter Sentimental Analysis</title>
 
+    <!-- STYLESHEETS -->
+    <link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet" type="text/css">
+
   </head>
   <body>
-    <div class="searchPanel">
-      <form class="searchBar" action="index.html" method="post">
-        <input type="text" name="name" value="" placeholder="Enter hashtag here..">
+    <!-- START CONTAINER-FLUID -->
+    <div class="container-fluid">
 
-      </form>
+      <!-- START ROW 1 -->
+      <div class="row-fluid">
+
+        <!-- START COL-XS-1 -->
+        <div class="col-md-1">
+
+          <!-- START SEARCH PANEL -->
+          <div class="searchPanel">
+            <form action="index.html" method="post">
+              <div class="form-group">
+              <input type="text" class="form-control" name="name" id="hashtag" value="" placeholder="Enter hashtag here..">
+              </div>
+            </form>
+          </div>
+          <!-- END SEARCH PANEL -->
+
+        </div>
+        <!-- START COL-XS-1 -->
+      </div>
+      <!-- END ROW 1 -->
+
+      <!-- START ROW 2 -->
+      <div class="row">
+
+        <!-- START COL-MD-6 -->
+        <div class="col-md-6">
+
+          <!-- START EMOTION GRAPH -->
+          <div class="emotionGraph">
+
+          </div>
+          <!-- END EMOTION GRAPH -->
+
+        </div>
+        <!-- END COL-MD-6 -->
+
+        <!-- START COL-MD-6 -->
+        <div class="col-md-6">
+          <!-- START TWITTER FEED -->
+          <div class="twitterFeed">
+
+          </div>
+          <!-- END TWITTER FEED -->
+        </div>
+        <!-- END COL-MD-6 -->
+
+      </div>
+      <!-- END ROW 2 -->
+
     </div>
-
-    <div class="emotionGraph">
-
-    </div>
-
-    <div class="twitterFeed">
-
-    </div>
+    <!-- END CONTAINER FLUID -->
 
     <footer>
 
