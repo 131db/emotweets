f702525dc37057ffd9d9f29a6923f74036545a3b
Mike Dayupay <mike.dayupay03@gmail.com>
2015-05-26 11:37:44 +0800

    Created index.php which will serve as the main. Added bootstrap files.

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Twitter Sentimental Analysis</title>
+
+  </head>
+  <body>
+    <div class="searchPanel">
+      <form class="searchBar" action="index.html" method="post">
+        <input type="text" name="name" value="" placeholder="Enter hashtag here..">
+
+      </form>
+    </div>
+
+    <div class="emotionGraph">
+
+    </div>
+
+    <div class="twitterFeed">
+
+    </div>
+
+    <footer>
+
+    </footer>
+
+  </body>
+</html>
