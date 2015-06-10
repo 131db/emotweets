618702075857bba1641dd47acec3d010e7ff4e60
Kim Blanquera <kim.blanquera@gmail.com>
2015-06-10 13:28:16 +0800

    Main NB algo

--- naivebayes.php
+++ naivebayes.php
@@ -2,6 +2,53 @@
 
   // NAIVE BAYES IMPLEMENTATION
 
-  
+  /*
+
+    P(A|B) = (A & B) / B
+
+
+
+  */
+
+  function calcProb($AB, $B) {
+
+    $p = ($AB / $B);
+
+    return $p;
+
+  }
+
+
+  function writeProb($file, $class, $prob) {
+
+    // write to file and add prob  or add to db
+
+  }
+
+  function getProbablities($file, $class) {
+
+    // read from file
+    $count = 0;
+
+    while() {
+
+      if($classFromFile == $class) {
+
+        if($wordFromFile == $word) {
+
+          $count++;
+
+        }
+
+      }
+
+      $prob = calcProb($count, 2500);
+
+      writeProb($file, $class, $prob);
+
+    }
+
+  }
+
 
 ?>
