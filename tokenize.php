ec5b2b5dd2c2d26ad7db2534557917a6619aac00
Kim Blanquera <kim.blanquera@gmail.com>
2015-06-11 13:15:29 +0800

    Sample tokenizing

--- tokenize.php
+++ tokenize.php
@@ -1,5 +1,19 @@
 <?php
 
+  include ('C:/xampp/htdocs/emotweets/php-nlp-tools-master/php-nlp-tools-master/autoloader.php');
+  use \NlpTools\Tokenizers\WhitespaceTokenizer;
 
+  $str = "Lorem ipsum sit dolor amet";
+
+  $spaceTok = new WhitespaceTokenizer();
+  $result = $spaceTok->tokenize($str);
+  $total = count($result);
+
+
+  for ($c = 0; $c < $total; $c++) {
+
+    echo $result[$c] . "<br>";
+
+  }
 
 ?>
