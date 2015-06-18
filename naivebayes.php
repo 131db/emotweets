ca6e5c84aaeb01fbe9a818417bc50100dc410d92
Mike Dayupay <mike.dayupay03@gmail.com>
2015-06-18 16:21:23 +0800

    Finish implementing the naive bayes algo for calculating the probability of tweet being positive or negative

--- naivebayes.php
+++ naivebayes.php
@@ -1,75 +1,152 @@
 <?php
 
+  require 'connect.php';
+
+
   // NAIVE BAYES IMPLEMENTATION
 
   /*
-
+    P(class) = (tweets | class) / no. of tweets
     P(A|B) = (A & B) / B
+    P(word | class) = count(word, class) + 1/ count(total words | class) + |V|
 
 
 
   */
+  $chinese = 3;
+  $japan = 1;
+  $totalTweets = 4;
+  $totalWords = 8;
+  $vocabSize = 6;
+  $chineseC = 5;
+  $chineseJ = 1;
+  $tokyoC = 0;
+  $japanC = 0;
+  $tokyoJ = 1;
+  $japanJ = 1;
 
-  function calcProb($AB, $B) {
+  function calcPrior($numOfClass, $totalTweets) {
+    $prior = $numOfClass / $totalTweets;
 
-    $p = ($AB / $B);
-
-    return $p;
-
+    return $prior;
   }
 
+  function calcProb($wordCount, $totalWords, $vocabSize) {
+    echo $wordCount . "<br>";
 
-  function writeProb($file, $class, $prob) {
+    $probability = ($wordCount + 1) / ($totalWords + $vocabSize);
 
-    // write to file and add prob  or add to db
-
+    return $probability;
   }
 
-  function getProbablities($file, $class) {
+  // function for calculating the probability of a tweet be positive/negative using naive bayes algorithm
+  function calcNaiveBayes($classPrior) {
 
-    // read from file
-    $count = 0;
+    global $con;
 
-    while() {
+    $classProbability = $classPrior;
+    $tokenizedTweet = array(
+      "Chinese",
+      "Chinese",
+      "Chinese",
+      "Tokyo",
+      "Japan"
+    );
 
-      if($classFromFile == $class) {
+    $arrayCount = array_count_values($tokenizedTweet); // key value pair on how many times the word has appeared in the tweet.
 
-        if($wordFromFile == $word) {
+    foreach($arrayCount as $word => $count) {
 
-          $count++;
+        $queryWordCount = 'SELECT chinese FROM wordbank WHERE word ="' . $word . '"'; //query for the number of times the word has appear in the given class
 
+        $queryVocabSize = 'SELECT count(word) AS "total" FROM wordbank'; //query for how many words are there in the vocabulary
+
+        $queryTotalWords = 'SELECT SUM(chinese) AS "chinese" FROM wordbank'; // query for getting the sum of appearances of each word in the given class
+        
+        $result2 = $con->query($queryVocabSize);
+        $result3 = $con->query($queryTotalWords);
+        $totalWords = $result3->fetch_assoc();
+        $vocabSize = $result2->fetch_assoc();
+        $result = $con->query($queryWordCount);
+
+        if($result->num_rows > 0) {
+          $row = $result->fetch_assoc();
+
+          $wordProb = calcProb($row["chinese"], $totalWords["chinese"], $vocabSize["total"]);
+
+          $classProbability *= pow($wordProb, $count);
+
+        }else {
+          echo "Query failed";
         }
-
-      }
-
-      $prob = calcProb($count, 2500);
-
-      writeProb($file, $class, $prob);
-
     }
+
+    return $classProbability;
 
   }
 
-  function classifyNew($newTweet) {
+  $chinesePrior = calcPrior($chinese, $totalTweets);
+  $japanPrior = calcPrior($japan, $totalTweets);
 
-    $total = count($newTweet);
+  echo calcNaiveBayes($chinesePrior) . "<br>";
 
-    for($i = 0; $i < $total; $i++) {
 
-      // read from file
 
-      if($wordFromBank == $newTweet[$i]) {
 
-        // get prob from file
 
-      }
-      else {
-        // new instance
-      }
 
-    }
-
-  }
+  // function writeProb($file, $class, $prob) {
+  //
+  //   // write to file and add prob  or add to db
+  //
+  // }
+  //
+  // function getProbablities($file, $class) {
+  //
+  //   // read from file
+  //
+  //   $count = 0;
+  //
+  //   while() {
+  //
+  //     if($classFromFile == $class) {
+  //
+  //       if($wordFromFile == $word) {
+  //
+  //         $count++;
+  //
+  //       }
+  //
+  //     }
+  //
+  //     $prob = calcProb($count, 2500);
+  //
+  //     writeProb($file, $class, $prob);
+  //
+  //   }
+  //
+  // }
+  //
+  // function classifyNew($newTweet) {
+  //
+  //   $total = count($newTweet);
+  //
+  //   for($i = 0; $i < $total; $i++) {
+  //
+  //     // read from file
+  //
+  //     if($wordFromBank == $newTweet[$i]) {
+  //
+  //       // get prob from file
+  //
+  //     }
+  //     else {
+  //       // new instance
+  //     }
+  //
+  //   }
+  //
+  // }
 
 
 ?>
