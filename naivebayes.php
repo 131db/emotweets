26715f13ebae412ab7006e0752173a00fc8bdbf8
Mike Dayupay <mike.dayupay03@gmail.com>
2015-06-19 15:02:48 +0800

    Fixed the naive bayes algo. Added a compare function to compare the positive and negative values.

--- naivebayes.php
+++ naivebayes.php
@@ -3,48 +3,47 @@
   require 'connect.php';
 
 
-  // NAIVE BAYES IMPLEMENTATION
+  # NAIVE BAYES CLASSIFICATION IMPLEMENTATION
 
   /*
+    Algorithm
     P(class) = (tweets | class) / no. of tweets
-    P(A|B) = (A & B) / B
     P(word | class) = count(word, class) + 1/ count(total words | class) + |V|
-
-
-
   */
-  $chinese = 3;
-  $japan = 1;
-  $totalTweets = 4;
-  $totalWords = 8;
-  $vocabSize = 6;
-  $chineseC = 5;
-  $chineseJ = 1;
-  $tokyoC = 0;
-  $japanC = 0;
-  $tokyoJ = 1;
-  $japanJ = 1;
 
+
+  # function for calculating the class prior for positive/negative
   function calcPrior($numOfClass, $totalTweets) {
     $prior = $numOfClass / $totalTweets;
 
     return $prior;
   }
 
+  # function for calculating the word conditional probability in a given class
   function calcProb($wordCount, $totalWords, $vocabSize) {
-    echo $wordCount . "<br>";
+
 
     $probability = ($wordCount + 1) / ($totalWords + $vocabSize);
 
     return $probability;
   }
 
-  // function for calculating the probability of a tweet be positive/negative using naive bayes algorithm
-  function calcNaiveBayes($classPrior) {
+  # Calculates the probability of the tweet being classified as positive
+  function calcPositiveNaiveBayes() {
 
     global $con;
 
-    $classProbability = $classPrior;
+    $queryTotalTweets = 'SELECT count(tweet) AS "totalTweets" FROM tweets';
+    $resultTotalTweets = $con->query($queryTotalTweets);
+    $total = $resultTotalTweets->fetch_assoc();
+
+
+    $queryTotalPositive = 'SELECT count(sentiment) AS "positive" FROM tweets WHERE sentiment = "Positive"';
+    $resultTotalPositive = $con->query($queryTotalPositive);
+    $totalPositive = $resultTotalPositive->fetch_assoc();
+
+    $classProbability = calcPrior($totalPositive["positive"], $total["totalTweets"]);
+
     $tokenizedTweet = array(
       "Chinese",
       "Chinese",
@@ -53,16 +52,16 @@
       "Japan"
     );
 
-    $arrayCount = array_count_values($tokenizedTweet); // key value pair on how many times the word has appeared in the tweet.
+    $arrayCount = array_count_values($tokenizedTweet); # key value pair on how many times the word has appeared in the tweet.
 
     foreach($arrayCount as $word => $count) {
 
-        $queryWordCount = 'SELECT chinese FROM wordbank WHERE word ="' . $word . '"'; //query for the number of times the word has appear in the given class
+        $queryWordCount = 'SELECT positiveCount FROM vocab WHERE word ="' . $word . '"'; # query for the number of times the word has appear in the given class
 
-        $queryVocabSize = 'SELECT count(word) AS "total" FROM wordbank'; //query for how many words are there in the vocabulary
+        $queryVocabSize = 'SELECT count(word) AS "total" FROM vocab'; # query for how many words are there in the vocabulary
 
-        $queryTotalWords = 'SELECT SUM(chinese) AS "chinese" FROM wordbank'; // query for getting the sum of appearances of each word in the given class
-        
+        $queryTotalWords = 'SELECT SUM(positiveCount) AS "positve" FROM vocab'; # query for getting the sum of appearances of each word in the given class
+
         $result2 = $con->query($queryVocabSize);
         $result3 = $con->query($queryTotalWords);
         $totalWords = $result3->fetch_assoc();
@@ -72,7 +71,7 @@
         if($result->num_rows > 0) {
           $row = $result->fetch_assoc();
 
-          $wordProb = calcProb($row["chinese"], $totalWords["chinese"], $vocabSize["total"]);
+          $wordProb = calcProb($row["positiveCount"], $totalWords["positive"], $vocabSize["total"]);
 
           $classProbability *= pow($wordProb, $count);
 
@@ -85,10 +84,83 @@
 
   }
 
-  $chinesePrior = calcPrior($chinese, $totalTweets);
-  $japanPrior = calcPrior($japan, $totalTweets);
+  # Calculates the probability of the tweet being classified as negative
+  function calcNegativeNaiveBayes() {
 
-  echo calcNaiveBayes($chinesePrior) . "<br>";
+    global $con;
+
+    $queryTotalTweets = 'SELECT count(tweet) AS "totalTweets" FROM tweets';
+    $resultTotalTweets = $con->query($queryTotalTweets);
+    $total = $resultTotalTweets->fetch_assoc();
+    
+
+    $queryTotalNegative = 'SELECT count(sentiment) AS "negative" FROM tweets WHERE sentiment = "Negative"';
+    $resultTotalNegative = $con->query($queryTotalNegative);
+    $totalNegative = $resultTotalNegative->fetch_assoc();
+
+    $classProbability = calcPrior($totalNegative["negative"], $total["totalTweets"]);
+
+    $tokenizedTweet = array(
+      "Chinese",
+      "Chinese",
+      "Chinese",
+      "Tokyo",
+      "Japan"
+    );
+
+    $arrayCount = array_count_values($tokenizedTweet); # key value pair on how many times the word has appeared in the tweet.
+
+    foreach($arrayCount as $word => $count) {
+
+        $queryWordCount = 'SELECT negativeCount FROM vocab WHERE word ="' . $word . '"'; # query for the number of times the word has appear in the given class
+
+        $queryVocabSize = 'SELECT count(word) AS "total" FROM vocab'; # query for how many words are there in the vocabulary
+
+        $queryTotalWords = 'SELECT SUM(negativeCount) AS "negative" FROM vocab'; # query for getting the sum of appearances of each word in the given class
+
+        $result2 = $con->query($queryVocabSize);
+        $result3 = $con->query($queryTotalWords);
+        $totalWords = $result3->fetch_assoc();
+        $vocabSize = $result2->fetch_assoc();
+        $result = $con->query($queryWordCount);
+
+        if($result->num_rows > 0) {
+          $row = $result->fetch_assoc();
+
+          $wordProb = calcProb($row["negativeCount"], $totalWords["negative"], $vocabSize["total"]);
+
+          $classProbability *= pow($wordProb, $count);
+
+        }else {
+          echo "Query failed";
+        }
+    }
+
+    return $classProbability;
+
+  }
+
+  /* compare the negative and positive probability value of the tweet
+    returns "Positive" if positive is greater than negative and "Negative" if
+    negative is greater than positive.
+  */
+  function compareValues($positiveValue, $negativeValue) {
+
+
+    if($positiveValue > $negativeValue) {
+      return "Positive";
+    } elseif ($negativeValue > $positiveValue) {
+      return "Negative";
+    } else {
+      return "Neutral";
+    }
+
+  }
+
+  echo calcPositiveNaiveBayes() . "<br>";
+  echo calcNegativeNaiveBayes() . "<br>";
+
+  echo compareValues(calcPositiveNaiveBayes(), calcNegativeNaiveBayes());
 
 
 
