85b9d1a9bc0d68e1dd4906b4281ba6f8ddfe8b02
Mike Dayupay <mike.dayupay03@gmail.com>
2015-06-25 01:06:21 +0800

    Added connect.php to connect to the database using mysqli

---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+
+	// CONNECTS TO DB
+
+	$host = "localhost";
+	$username = "root";
+	$pw = "1234";
+	$db = "emotweets";
+
+	$con = new mysqli($host, $username, $pw, $db);
+
+	if($con->connect_error) {
+		die("Connection failed: " . $con->connect_error);
+	}
+
+	
+
+?>
